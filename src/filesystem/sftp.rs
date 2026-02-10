18d11c9298146428aaf10f9b68509b5c91215ad4
caelansar <819711623@qq.com>
2026-02-10 21:02:19 +0800

    perf: streamline directory and file type determination for SFTP entries by consolidating symlink handling

--- src/filesystem/sftp.rs
+++ src/filesystem/sftp.rs
@@ -117,25 +117,16 @@ impl FileSystem for SftpFileSystem {
 
             let file_type = entry.file_type();
 
-            // Determine if this is a directory
-            // For symlinks, we need to follow them to check if they point to directories
-            let is_dir = if file_type.is_symlink() {
-                // Try to follow the symlink to get the target's metadata
+            // Determine if this is a directory or file
+            // For symlinks, we need to follow them once to check the target's type
+            let (is_dir, is_file) = if file_type.is_symlink() {
+                // Try to follow the symlink to get the target's metadata (single call)
                 match self.session.metadata(full_path_str.as_ref()).await {
-                    Ok(target_metadata) => target_metadata.is_dir(),
-                    Err(_) => false, // If we can't follow the symlink, treat it as a file
+                    Ok(target_metadata) => (target_metadata.is_dir(), target_metadata.is_regular()),
+                    Err(_) => (false, false), // If we can't follow the symlink, treat as neither
                 }
             } else {
-                file_type.is_dir()
-            };
-
-            let is_file = if file_type.is_symlink() {
-                match self.session.metadata(full_path_str.as_ref()).await {
-                    Ok(target_metadata) => target_metadata.is_regular(),
-                    Err(_) => false, // If we can't follow the symlink, treat it as a file
-                }
-            } else {
-                file_type.is_file()
+                (file_type.is_dir(), file_type.is_file())
             };
 
             // Get file size from metadata
